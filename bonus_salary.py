0729707247cb3b7cabb60322f2c9670cd37935b6
Md Ansar choudhari <mdansarchoudry@gmail.com>
2025-11-11 09:22:10 +0530

    Create bonus_salary.py

---
+++ bonus_salary.py
@@ -0,0 +1,20 @@
+import sys
+
+if len(sys.argv)==2:
+    script_name=sys.argv[0]
+    salary=sys.argv[1]
+
+else:
+    salary=100000
+
+
+bonus=0.1*float(salary)
+
+total_salary=float(salary)+bonus
+
+
+
+
+print("Base Salary: ", salary)  
+print("Bonus: ", bonus)
+print("Total Salary: ", total_salary)
